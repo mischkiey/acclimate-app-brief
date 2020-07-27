0b45dbeb9eb3023df38a61f00227b2c87b32211b
mischkiey <mischkieycf@gmail.com>
2020-07-27 01:12:28 -0400

    Added api and token services, expecting modifications

--- acclimate-app-client/src/components/Nav/Nav.js
+++ acclimate-app-client/src/components/Nav/Nav.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import TokenService from '../../services/token-services';
 
+// Fix this eyesore, you can do better
 function Nav () {
+
+    const links = (TokenService.hasAuthToken())
+        ?
+        (<ul>
+            <li><NavLink activeClassName='' to='/searchpage'>Search Database</NavLink></li>
+            <li><NavLink activeClassName='' to='/'>Logout</NavLink></li>
+        </ul>)
+        :
+        (<ul>
+            <li><NavLink activeClassName='' to='/loginpage'>Login</NavLink></li>
+            <li><NavLink activeClassName='' to='/signuppage'>Sign up</NavLink></li>
+        </ul>)
+
     return (
         <nav>
-            <ul>
-                {/* For later: Render proper link when logged in or out by checking local storage for token*/}
-                <li><NavLink activeClassName='' to='/loginpage'>Login/Logout</NavLink></li>
-                <li><NavLink activeClassName='' to='/signuppage'>Sign up</NavLink></li>
-            </ul>
+            {/* Render proper links when logged in or out by checking local storage for token */}
+            {links}
         </nav>
     )
 }
