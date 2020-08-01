caccc916a492e9befee0c80232c2e2f02301eb48
mischkiey <mischkieycf@gmail.com>
2020-08-01 13:05:20 -0400

    Fixed git

--- acclimate-app-client/src/config.js
+++
@@ -1,4 +0,0 @@
-export default {
-    API_ENDPOINT: '',
-    API_TOKEN: 'acclimate-client-token',
-};
--- acclimate-app-client/src/contexts/AcclimateContext.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const AcclimateContext = React.createContext({});
-
-export default AcclimateContext;
